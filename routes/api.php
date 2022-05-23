bda43b73ac2ac46d11a5d692c76490428ba89a4e
reachea <reacheasambath@gmail.com>
2022-05-23 08:37:01 +0700

    setup sanctum auth

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Public
+Route::get('/products', function (Request $request) {
+    return 'product';
+});
+
+// Protected
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::get('/users', function (Request $request) {
+        return $request->user();
+    });
 });
